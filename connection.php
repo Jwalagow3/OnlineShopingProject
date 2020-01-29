d1781809c6efb8bbe48b86542c89a4393bd45679
Jwalagow3 <59638015+Jwalagow3@users.noreply.github.com>
2020-01-29 08:40:03 +0530

    Update connection.php

--- connection.php
+++ connection.php
@@ -1,4 +1,5 @@
 <?php
-$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
+$con=mysqli_connect("localhost","admin","admin","shop") or die(mysqli_error($con));
 //$con=mysqli_connect("localhost","root","","store") or die(mysqli_error($con));
 ?>
+
